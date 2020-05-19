ac863a6a0acc256b7cfb123127ef64d514dc9667
Andrew Plummer <plummer574@gmail.com>
2020-05-19 11:04:33 +0100

    Run all tests, report failure at the end

--- runTest.sh
+++ runTest.sh
@@ -7,15 +7,26 @@ if [[ -z $FAST_GLOB_VERSION ]]; then
     exit 1;
 fi
 
+SUCCESS=1
+
 echo "Installing fast-glob@FAST_GLOB_VERSION"
 npm install --no-save "fast-glob@$FAST_GLOB_VERSION"
 
+function setFailed() {
+    SUCCESS=
+}
+
 echo
 echo "Run npm run ts-node ./testSource.ts, expect to see all files"
-npm run ts-node ./testSource.ts
+npm run ts-node ./testSource.ts || setFailed
 echo
 echo "Run npm run tsc ./testSource.ts && node ./testSource.js, expect to see all files"
-npm run tsc ./testSource.ts && node ./testSource.js
+npm run tsc ./testSource.ts && node ./testSource.js || setFailed
 echo
 echo "Run npx ts-node ./testSource.ts, expect to see all files"
-npx ts-node ./testSource.ts
+npx ts-node ./testSource.ts || setFailed
+
+if [[ -z $SUCCESS ]]; then
+    echo "Some tests failed"
+    exit 1
+fi
