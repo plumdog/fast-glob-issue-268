1c64598e28f28ca61804f77772ed433a703c7704
Andrew Plummer <plummer574@gmail.com>
2020-05-19 10:49:49 +0100

    More useful output

--- testSource.ts
+++ testSource.ts
@@ -1,3 +1,10 @@
 import * as fg from 'fast-glob';
 
-fg(['a.*', 'b.txt', 'c.txt']).then(console.log);
+fg(['a.*', 'b.txt', 'c.txt']).then((results: Array<string>) => {
+    console.log(JSON.stringify(results));
+    if (results.length === 3) {
+        console.log('GOOD: found 3 files');
+    } else {
+        console.log(`BAD: found ${results.length} files`);
+    }
+});
